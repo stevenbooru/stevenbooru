d1ec4d359139be8b8e11a0f56f04562a12379a92
Christine Dodrill <xena@yolo-swag.com>
2015-08-01 08:52:49 -0700

    Add Unique ID of the user to the wrapper structure
    Also hostname of the node that served the user.

--- src/stevenbooru.cf/eye/wrapper.go
+++ src/stevenbooru.cf/eye/wrapper.go
@@ -1,6 +1,9 @@
 package eye
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 // Wrapper is the kind of data that templates will parse.
 //
@@ -8,17 +11,26 @@ import "net/http"
 // to show on the page.
 type Wrapper struct {
 	Username string
+	UID      string
 	Role     string
+	Hostname string
 	Data     interface{}
 }
 
 // Wrap wraps a given segment of data into a form that the other layers will
 // find acceptable.
 func Wrap(r *http.Request, data interface{}) *Wrapper {
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
 	w := &Wrapper{
 		Data:     data,
 		Username: r.Header.Get("username"),
+		UID:      r.Header.Get("uid"),
 		Role:     r.Header.Get("role"),
+		Hostname: hostname,
 	}
 
 	return w
