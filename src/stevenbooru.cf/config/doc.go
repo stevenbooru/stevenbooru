19338426e3b7fc56eec69af709e8f61ce0c45992
Christine Dodrill <xena@yolo-swag.com>
2015-07-31 17:53:38 -0700

    Add config and models package documentation

---
+++ src/stevenbooru.cf/config/doc.go
@@ -0,0 +1,5 @@
+/*
+Package config contains common configuration utilities and tools
+that are useful for stevenbooru.
+*/
+package config
---
+++ src/stevenbooru.cf/models/doc.go
@@ -0,0 +1,5 @@
+/*
+Package models contains data structures and other models that Stevenbooru uses
+in common with many components.
+*/
+package models
