0f5d2eeba3ee8d4c40de2107141d06368cb95e8b
Christine Dodrill <xena@yolo-swag.com>
2015-07-31 19:22:30 -0700

    Render a basic, single page

--- src/stevenbooru.cf/cmd/stevenbooru/main.go
+++ src/stevenbooru.cf/cmd/stevenbooru/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Xe/middleware"
+	"github.com/codegangsta/negroni"
+	"github.com/drone/routes"
+	"stevenbooru.cf/config"
+	"stevenbooru.cf/eye"
+)
+
+var (
+	c config.Config
+
+	configFileFlag = flag.String("conf", "./cfg/stevenbooru.cfg", "configuration file to load")
+)
+
+func init() {
+	flag.Parse()
+
+	var err error
+	c, err = config.ParseConfig(*configFileFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world! This is a placeholder.")
+	mux := routes.New()
+
+	mux.Get("/", func(rw http.ResponseWriter, r *http.Request) {
+		eye.DoTemplate("views/index", rw, r, nil)
+	})
+
+	n := negroni.Classic()
+
+	middleware.Inject(n)
+	n.UseHandler(mux)
+
+	n.Run(fmt.Sprintf("%s:%s", c.HTTP.Bindhost, c.HTTP.Port))
 }
